d2ff7ba708d12bc68788e1528eaf5fd1fc00ad73
nick-symon <symon.nick@gmail.com>
2020-09-21 12:20:31 -0500

    code challenge updates

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -14,11 +14,18 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        # TODO: Flesh out this method to parse an address string using the
-        # parse() method and return the parsed components to the frontend.
-        return Response({})
+        input_string = request.GET['address']
+        try:
+            address_components, address_type = self.parse(input_string)
+        except usaddress.RepeatedLabelError:
+            raise ParseError(detail='Unable to parse this value due to repeated labels. Please correct and resubmit')
+
+        return Response({
+            'input_string': input_string,
+            'address_components': address_components,
+            'address_type': address_type
+        })
 
     def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
-        return address_components, address_type
+        address_parse = usaddress.tag(address)
+        return address_parse
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,22 @@
 import pytest
+from urllib.parse import urlencode
+import usaddress
 
 
 def test_api_parse_succeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
-    # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    test_address_components, test_address_type = usaddress.tag(address_string)
+    get_param = urlencode({'address': address_string}) 
 
+    response = client.get(f'/api/parse/?{get_param}')
+    assert response.status_code == 200
+    assert response.data['input_string'] == '123 main st chicago il' 
+    assert response.data['address_components'] == test_address_components
+    assert response.data['address_type'] == test_address_type
 
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    get_param = urlencode({'address': address_string}) 
+    response = client.get(f'/api/parse/?{get_param}')
+    assert response.status_code == 400
+    assert response.data['detail'] == "Unable to parse this value due to repeated labels. Please correct and resubmit"
