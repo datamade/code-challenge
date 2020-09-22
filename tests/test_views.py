d278f1b0e9af5e3881ed726bd5b192207ee27997
J. Heasly <jheasly@earthlink.net>
2020-09-21 23:27:04 -0500

    The DataMade documentation on the tests portion is extrememly bad.

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,21 @@
 import pytest
-
+from django.test import Client
 
 def test_api_parse_succeds(client):
     # TODO: Finish this test. Send a request to the API and confirm that the
     # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    c = Client()
+    response = c.get('/api/parse/', {'address': address_string})
+    assert response.status_code == 200
+    # pytest.fail()
 
 
 def test_api_parse_raises_error(client):
     # TODO: Finish this test. The address_string below will raise a
     # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    c = Client()
+    response = c.get('/api/parse/', {'address': address_string})
+    assert response.status_code == 400
+    # pytest.fail()
