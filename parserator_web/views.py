dcc79477f5456cb3836d5d1f22e096aa7257e639
Charmaine Runes <charmaine.runes@gmail.com>
2020-09-22 22:56:45 -0500

    tasks 1 and 2 complete

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,9 +16,31 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        try:
+            input_string = request.data
+            address_components, address_type = self.parse(input_string)
+
+            if (address_components is None) or (address_type is None):
+                return None
+            else:
+                content = (input_string, address_components, address_type)
+
+        except ParseError:
+            print("Malformed data")
+            return None
+
+        return Response(content)
+
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+
+        try:
+            address_components, address_type = usaddress.tag(address)
+
+        except usaddress.RepeatedLabelError as e:
+            print(e.orginal_string, "is not a valid address for tagging")
+            return None, None
+
         return address_components, address_type
