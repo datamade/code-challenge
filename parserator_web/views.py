3c50a435c3dc8e00b9e654e4159077d44f2bed0b
frankiesastronautfund <grotton.2@buckeyemail.osu.edu>
2020-09-22 23:29:05 -0400

    complete tasks

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -5,7 +5,6 @@ from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
 from rest_framework.exceptions import ParseError
 
-
 class Home(TemplateView):
     template_name = 'parserator_web/index.html'
 
@@ -16,9 +15,16 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+
+        address_components, address_type = self.parse(request.query_params.get("address"))
+        return Response({"input_string": request.query_params.get("address"), "address_components": address_components, "address_type": address_type})
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+        try:
+            address_components, address_type = usaddress.tag(address)
+        except:
+            raise ParseError()
+
         return address_components, address_type
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,18 @@
 import pytest
 
-
 def test_api_parse_succeds(client):
     # TODO: Finish this test. Send a request to the API and confirm that the
     # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    res = client.get("/api/parse", {"address": address_string})
+    assert res.status_code == 200
 
 
 def test_api_parse_raises_error(client):
     # TODO: Finish this test. The address_string below will raise a
     # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    res = client.get("/api/parse", {"address": address_string})
+    assert res.status_code == 500
+
+## These tests don't work, I don't have a lot of experience with Django and I ran out of time, sorry!
