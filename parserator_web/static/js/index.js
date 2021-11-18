2e5c32c04e696115261cdaf7c39364e305aacdcc
Garrett Julaka <gnjulaka@gmail.com>
2021-11-18 12:37:08 -0600

    Linting passed.

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,42 +1,54 @@
 /* TODO: Flesh this out to connect the form to the API and render results
-   in the #address-results div. */
+in the #address-results div. */
 
-   const fillAddressResults = (data) => {
-      document.getElementById('address-results').style = "display:block";
-      tblbdy = document.querySelector('table.table tbody');
+'use strict'
 
-      for ( let i = 0; i < 2; i++) {
-         let row = document.createElement("tr");
+fillAddressResults = function (data) {
 
-         for ( let j = 0; j < 2; j++) {
-            let cell = document.createElement("td");
-            let cellText = document.createTextNode("Test"+i)
+  // Unhide the results table and grab the table element for results filling
+  document.getElementById('address-results').style = "display:block"
+  tblbdy = document.querySelector('table.table tbody')
 
-            cell.appendChild(cellText)
-            row.appendChild(cell)
-         }
-
-         tblbdy.appendChild(row)
-      }
+  keys = Object.keys(data['address_components'])
+  componentLength = keys.length
 
 
-   }
+  for (var i = 0; i < componentLength; i++) {
+
+    // A bit verbose, but address component length will be variable based on user input
+    // loop through all of the components and fill the table
+
+    var row = document.createElement("tr")
+    var addressCell = document.createElement("td")
+    var addressPartText = document.createTextNode(data['address_components'][keys[i]])
+    var tagCell = document.createElement("td")
+    var tagPartText = document.createTextNode(keys[i])
+
+    addressCell.appendChild(addressPartText)
+    tagCell.append(tagPartText)
+    row.appendChild(addressCell)
+    row.appendChild(tagCell)
+
+    tblbdy.appendChild(row)
+  }
+}
  
 
-   document.getElementById('submit').addEventListener('click', function(event) {
+// Add the API call to the submit button from the form
+document.getElementById('submit').addEventListener('click',function (event) {
 
-      event.preventDefault();
+  event.preventDefault()
 
-      $.ajax({
-         url: 'http://localhost:8000/api/parse',
-         datatype: 'json',
-         type: 'GET',
-         data: {
-            input_string: document.getElementById('address').value
-         },
-         success: function(data) {
-            fillAddressResults(data)
-         }
-      })
+  $.ajax({
+    url: 'http://localhost:8000/api/parse',
+    datatype: 'json',
+    type: 'GET',
+    data: {
+      input_string: document.getElementById('address').value
+    },
+    success: function (data) {
+      fillAddressResults(data)
+    }
+  })
 
-   })
+})
