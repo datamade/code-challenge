c3a4b32d1726bf7f3101c86d624080b87d4199ff
Victoria Lam <27948576+Wizardess@users.noreply.github.com>
2021-11-29 17:47:32 -0800

    Code challenge steps 1-4

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,69 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+
+jquery('form').on('submit', function(event){
+   /** Connect the HTML form to the API and render results in the
+   #address-results div. */
+
+   // Prevent the page from reloading
+   event.preventDefault();
+
+   // Clear data from previous queries from the page
+   jquery('tbody').html('')
+   jquery('#address-results').hide()
+   jquery('#error-message').hide()
+   
+   // Load form data into userInput as a JSON object
+   var userInput = jquery('form').serializeArray();
+
+   // * WIP: Catching bad inputs clientside
+   // if (jquery('#address').val == ''){
+   //    console.log('found it')
+   //    jquery('#error-message').html("Please no.")
+   //    jquery('#error-message').show()
+   //    return false;
+   // }
+
+   // GET request to parserator API
+   jquery.ajax({
+      method: 'GET',
+      url: '/api/parse/',
+      data: userInput,
+      success: function(response){
+
+         // Extract data from GET response object
+         var {address_components,address_type,http_error} = response
+
+         if (http_error !== undefined){
+            /** Catch exceptions stemming from usaddress */
+
+            // Show error message in the error message div
+            jquery('#error-message').html("Uh oh! That doesn't seem to be a valid address.")
+
+            // Hide the address component table
+            jquery('#error-message').show()
+
+         } else {
+            /**
+             * Display address components, their respective tags, and
+             * address type for street addresses or ambiguous addresses.*/
+
+            // Extract parsed address components from GET response
+            var {address_components,address_type} = response
+
+            /* Fill the address component table: For each address component, 
+            append a row to the table with the component and its tag. */
+            jquery.each(address_components, function(tag, component){
+               jquery('tbody').append('<tr><td>'+component+'</td><td>'+tag+'</td></tr>')
+            })
+            
+            // Show filled address component table
+            jquery('#address-results').show();
+            
+            // Display address type in parse-type
+            jquery('#parse-type').html(address_type)
+
+         }
+      }
+   })
+})
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -14,11 +14,42 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
+        """Returns the string the user sent, the parsed address
+        components and the address type if the string is a valid address"""
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+
+        # Get the unparsed address from JSON object query
+        input_string = request.query_params['address']
+
+        try:
+
+            # Get address components using .parse() method
+            address_components, address_type = self.parse(input_string)
+
+            # Return a dictionary object
+            return Response({
+                'input_string': input_string,
+                'address_components': address_components,
+                'address_type': address_type
+            })
+
+        except:
+
+            # Return the expected dictionary object, but include error info
+            return Response({
+                'input_string': input_string,
+                'address_components': '',
+                'address_type': 'Invalid',
+                'http_error': '400'
+            })
 
     def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
+        """Returns parsed components and address type of a given address 
+        using usaddress"""
+
+        # Use .tag() from usaddress to parse address
+        address_components, address_type = usaddress.tag(address)
+
+        # Return address_components as OrderedDict and address_type as Str
         return address_components, address_type
