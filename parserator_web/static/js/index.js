9fca7b6aa8b4956c9b87353ec8567de5087143db
t hawks <thawks@ts-MacBook-Pro.local>
2021-12-07 23:14:25 -0500

    challenge submission

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,67 @@
-/* TODO: Flesh this out to connect the form to the API and render results
-   in the #address-results div. */
+// set form action
+let form = document.querySelector("form");
+form.action = "api/parse/";
+
+// define references for hidden results section, as well as parse-type element
+let resultsSection = document.getElementById("address-results");
+let parseType = document.getElementById("parse-type");
+
+// define functions
+function GET_url_with_body(form){
+   // assembles url from html form for GET request with body
+   let params = new URLSearchParams(new FormData(form));
+   let urlString = form.action.toString() + "?" + params.toString();
+   return urlString;
+}
+
+function fillTable(parsedAddress) {
+   // parses parsedAddress dict from GET response into 
+   // viewable table
+   let tb = document.querySelector("tbody");
+   tb.innerHTML = "";
+   for (const [addressPart, tag] of Object.entries(parsedAddress)){
+      // console.log(`${key}: ${value}`)
+      var row = document.createElement("tr");
+      for (let i of [addressPart, tag]){
+         var cell = document.createElement("td");
+         var cellText = document.createTextNode(i);
+         cell.appendChild(cellText);
+         row.appendChild(cell);
+      }
+      tb.appendChild(row);
+   }
+}
+
+function parseAlert() {
+   // create <div> for alert in the event parser returns an error
+   var alert_div = document.createElement("div");
+   alert_div.className = "alert alert-danger";
+   var text = "Address parse failed. Did your submission included any repeated elements?";
+   var alert_text = document.createTextNode(text);
+   alert_div.appendChild(alert_text);
+   resultsSection.insertAdjacentElement('beforebegin', alert_div);
+}
+
+// add listener to form, collecting data to send to the GET
+// then process the API response, either populating the results in a table 
+// or throwing an error.
+form.addEventListener('submit', (event) => {
+   event.preventDefault();
+   var urlString = GET_url_with_body(event.target);
+   fetch(urlString)
+      .then(response => {
+         if (!response.ok) {
+            throw new Error(`Server Error; code: ${response.code}`);
+         }
+         return response.json();
+      }).then((data) => {
+         if (data["status" == "success"]) {
+            parseType.textContent = data["address_type"];
+            fillTable(data["tagged_address"]);
+            resultsSection.style.display = 'table';
+         } else if (data["status"] == "repeatedLabelError") {
+            parseAlert(data["original_string"], data["parsed_string"]);
+         }
+      });
+});
+
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -14,11 +14,35 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        # TODO: Flesh out this method to parse an address string using the
-        # parse() method and return the parsed components to the frontend.
-        return Response({})
+        """
+        API endpoint to handle requests to parse address strings.
+        Returns
+        - input string,
+        - parsed address (tagged by component),
+        - address type as JSON
+        Except when parse() passes a ReapetedLabelError
+        """
+        input_string = request.GET.get('address')
+        try:
+            tagged_address, address_type = self.parse(input_string)
+            return Response({
+                "status": "success",
+                "tagged_address": tagged_address,
+                "address_type": address_type,
+                "input_string": input_string,
+            })
+        except usaddress.RepeatedLabelError as rle:
+            return Response({
+                "status": "repeatedLabelError",
+                "original_string": rle.original_string,
+                "parsed_string": rle.parsed_string,
+            })
 
-    def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
+    def parse(self, input_string):
+        """
+        passes input_string to usaddress.tag(), returning a tuple containing:
+        - parsed and tagged string as OrderedDict: {'AddressNumber': '123'}
+        - a string describing the address type
+        """
+        address_components, address_type = usaddress.tag(input_string)
         return address_components, address_type
