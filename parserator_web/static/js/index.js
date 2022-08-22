d5bfdfef963380572934ed32252915aee31e4549
TayTufenkjian <taytuf@gmail.com>
2022-08-22 16:13:22 -0400

    Wire up the form to send requests to the API
    Step 3 of the challenge

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,14 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+
+document.addEventListener('DOMContentLoaded', () => {
+   const addressForm = document.querySelector('form');
+   addressForm.onsubmit = () => {
+      let addressInput = document.querySelector('#address').value;
+      fetch(`api/parse/?input=${addressInput}`)
+      .then(response => response.json())
+      .then(data => {
+         console.log(data);
+      });
+   }
+});
