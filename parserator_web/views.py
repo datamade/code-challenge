184379e78813950ac5a5b33182676cab4c4daa85
TayTufenkjian <taytuf@gmail.com>
2022-08-22 12:03:49 -0400

    Complete the API endpoint
    Step 2 of the challenge

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,7 +16,18 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        input = request.data['address']
+        try:
+            parsed_address = self.parse(input)
+            return Response({
+                'input_string': input,
+                'address_components': parsed_address[0],
+                'address_type': parsed_address[1]
+            })
+        except:
+            return Response({
+                'error_message': 'Error: We cannot parse that address.' 
+            })
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
