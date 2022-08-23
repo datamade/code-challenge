f7e2fd4d611355b368ceeeb47c410aeda60dacf9
TayTufenkjian <taytuf@gmail.com>
2022-08-23 09:21:30 -0400

    Fix JS linting errors

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,69 +1,71 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
 
-document.addEventListener('DOMContentLoaded', function() {
+'use strict'
 
-   // Get the relevant page elements
-   const addressForm = document.querySelector('form');
-   const addressField = document.querySelector('#address');
-   const addressResults = document.querySelector('#address-results');
-   const parseType = document.querySelector('#parse-type');
-   const addressTableBody = document.querySelector('tbody');
+document.addEventListener('DOMContentLoaded', function () {
+   
+  // Get the relevant page elements
+  var addressForm = document.querySelector('form')
+  var addressField = document.querySelector('#address')
+  var addressResults = document.querySelector('#address-results')
+  var parseType = document.querySelector('#parse-type')
+  var addressTableBody = document.querySelector('tbody')
 
-   // Parse and display address or error when form is submitted
-   addressForm.onsubmit = () => {
-      let addressInput = document.querySelector('#address').value;
+  // Parse and display address or error when form is submitted
+  addressForm.onsubmit = function () {
+    var addressInput = document.querySelector('#address').value
 
-      // Clear the form field for the next attempt
-      addressField.value = '';
-      // Clear any prior errors
-      if (document.querySelector('#error')) {
-         document.querySelector('#error').remove();
+    // Clear the form field for the next attempt
+    addressField.value = ''
+    // Clear any prior errors
+    if (document.querySelector('#error')) {
+      document.querySelector('#error').remove()
+    }
+    // Clear any prior results 
+    if (addressResults.style.display === 'block') {
+      addressResults.style.display === 'none'
+      var rows = addressTableBody.getElementsByTagName('tr')
+      // Convert the NodeList to an array to avoid looping over a shrinking NodeList
+      var rowsArray = Array.prototype.slice.call(rows)
+      for (var i = 0; i < rowsArray.length; i++) {
+        rowsArray[i].remove()
       }
-      // Clear any prior results 
-      if (addressResults.style.display === 'block') {
-         addressResults.style.display === 'none';
-         let rows = addressTableBody.getElementsByTagName('tr');
-         // Convert the NodeList to an array to avoid looping over a shrinking NodeList
-         let rowsArray = [...rows];
-         for (let row of rowsArray) {
-            row.remove();
-         }
-      }
+    }
 
-      fetch(`api/parse/?input=${addressInput}`)
-      .then(response => response.json())
-      .then(data => {
-         if (data.error) {
-            // Display the error message
-            let errorDiv = document.createElement('div');
-            errorDiv.id = 'error';
-            let error = document.createElement('h4');
-            error.className = 'error';
-            error.textContent = `Error: ${data.error}`;
-            let message = document.createElement('p');
-            message.textContent = data.message;
-            errorDiv.append(error, message);
-            document.querySelector('.container').append(errorDiv);
-         } else {
-            // Display the address type and components table
-            let addressType = data.address_type;
-            parseType.textContent = addressType;
-            let addressComponents = data.address_components;
-            for (const component in addressComponents) {
-               let row = document.createElement('tr');
-               let addressPart = document.createElement('td');
-               addressPart.textContent = addressComponents[component];
-               let tag = document.createElement('td');
-               tag.textContent = component;
-               row.append(addressPart, tag);
-               addressTableBody.append(row);
-            }
-            // Unhide the results div
-            addressResults.style.display = 'block';
-         }
-      });
-      // Stop the form from actually submitting
-      return false;
-   }
-});
+    fetch('api/parse/?input=' + addressInput)
+      .then(function (response) {return response.json()})
+      .then(function (data) {
+        if (data.error) {
+          // Display the error message
+          var errorDiv = document.createElement('div')
+          errorDiv.id = 'error'
+          var error = document.createElement('h4')
+          error.className = 'error'
+          error.textContent = 'Error: ' + data.error
+          var message = document.createElement('p')
+          message.textContent = data.message
+          errorDiv.append(error, message)
+          document.querySelector('.container').append(errorDiv)
+        } else {
+          // Display the address type and components table
+          var addressType = data.address_type
+          parseType.textContent = addressType
+          var addressComponents = data.address_components
+          for (var component in addressComponents) {
+            var row = document.createElement('tr')
+            var addressPart = document.createElement('td')
+            addressPart.textContent = addressComponents[component]
+            var tag = document.createElement('td')
+            tag.textContent = component
+            row.append(addressPart, tag)
+            addressTableBody.append(row)
+          }
+          // Unhide the results div
+          addressResults.style.display = 'block'
+        }
+      })
+    // Stop the form from actually submitting
+    return false
+  }
+})
