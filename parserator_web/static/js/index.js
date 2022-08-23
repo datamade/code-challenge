415e361a9f68effe3eeb87918b50b6943ebcead7
Michelle Orden <morden@uchicago.edu>
2022-08-23 12:52:28 -0500

    Added some error handling in views.py. Need to check the get() method. Started connecting html and js to api but not working.

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,23 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+
+// add on click
+console.log("adding on click method")
+let submit_button = document.getElementById("submit");
+submit_button.addEventListener("click", parse_address());
+
+function parse_address() {
+   console.log("Parsing address");
+   // grab input data
+   let input_address = document.getElementById("address").value;
+
+   // call api
+   let request = fetch("",
+                       {method: 'POST',
+                        body: JSON.stringify({"address": input_address})});
+   
+   request.then((response) => response.json())
+          .then(data => {
+            // display data in html
+          })
+}
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,12 +16,33 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        print(request)
+        address = request.data["address"]
+
+        response = self.parse(address)
+        if response["status"] == 200:
+            return Response({"status": response["status"],
+                             "input_string": address,
+                             "address_components": response["address_components"],
+                             "address_type": response["address_type"]})
+        return Response({"status": 400,
+                         "input_string": response["original_string"],
+                         "address_components": response["parsed_string"],
+                         "address_type": "Unknown"})
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
 
-        address_components, address_type = usaddress.tag(address)
+        # https://usaddress.readthedocs.io/en/latest/
+        # https://developer.mozilla.org/en-US/docs/Web/HTTP/Status#client_error_responses
+        try:
+            address_components, address_type = usaddress.tag(address)
+        except usaddress.RepeatedLabelError as e:
+            return Response({"status": 400,
+                             "parsed_string": e.parsed_string,
+                             "original_string": e.original_string})
 
-        return address_components, address_type
+        return Response({"status": 200,
+                         "address_components": address_components,
+                         "address_type": address_type})
