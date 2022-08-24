0b010c3008dbeb77b2f4173446a051f9796e6856
charlieohara <34257389+charlieohara@users.noreply.github.com>
2022-08-24 15:33:44 -0500

    Code Challenge Completed

--- parserator_web/views.py
+++ parserator_web/views.py
@@ -3,7 +3,6 @@ from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.exceptions import ParseError
 
 
 class Home(TemplateView):
@@ -14,11 +13,16 @@ class AddressParse(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request):
-        # TODO: Flesh out this method to parse an address string using the
-        # parse() method and return the parsed components to the frontend.
-        return Response({})
+        input_string = request.GET["address"]
+        try:
+            address_components, address_type = self.parse(input_string)
+
+            return Response({"input_string": input_string,
+                             "address_components": address_components,
+                             "address_type": address_type})
+        except usaddress.RepeatedLabelError as err:
+            return Response({"address_type": f"Error: {err=}"})
 
     def parse(self, address):
-        # TODO: Implement this method to return the parsed components of a
-        # given address using usaddress: https://github.com/datamade/usaddress
+        address_components, address_type = usaddress.tag(address)
         return address_components, address_type
--- tests/test_views.py
+++ tests/test_views.py
@@ -1,15 +1,36 @@
-import pytest
+from django.urls import reverse
+
+
+def get_response(client, address_string):
+    url = reverse("address-parse")
+    url += "?address=" + "+".join(address_string.split())
+    return client.get(url)
 
 
 def test_api_parse_succeeds(client):
-    # TODO: Finish this test. Send a request to the API and confirm that the
-    # data comes back in the appropriate format.
     address_string = '123 main st chicago il'
-    pytest.fail()
+    response = get_response(client, address_string)
+    assert response.data == {"input_string": "123 main st chicago il",
+                             "address_components": {"AddressNumber": "123",
+                                                    "StreetName": "main",
+                                                    "StreetNamePostType": "st",
+                                                    "PlaceName": "chicago",
+                                                    "StateName": "il"},
+                             "address_type": "Street Address"}
 
 
 def test_api_parse_raises_error(client):
-    # TODO: Finish this test. The address_string below will raise a
-    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
     address_string = '123 main st chicago il 123 main st'
-    pytest.fail()
+    response = get_response(client, address_string)
+    assert response.data == {"address_type": "Error: "
+                                             "err=RepeatedLabelError('"
+                                             "123 main st chicago il 123 main st', "
+                                             "[('123', 'AddressNumber'), "
+                                             "('main', 'StreetName'), "
+                                             "('st', 'StreetNamePostType'), "
+                                             "('chicago', 'PlaceName'), "
+                                             "('il', 'StateName'), "
+                                             "('123', 'AddressNumber'), "
+                                             "('main', 'StreetName'), "
+                                             "('st', 'StreetNamePostType')],"
+                                             " 'AddressNumber')"}
