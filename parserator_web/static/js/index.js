c2935841815036bd32fe66dc9da50b2e2629ca33
Adam Callanan <a.callanan@sbcglobal.net>
2022-08-25 17:23:56 -0500

    begin adding script for form submission

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,8 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+
+   $('.form').on('submit', function () {
+      var address = document.getElementById('address').value;
+      alert(`Submitted form with: ${address}`);
+  });
+  
