b54dce585cc04560aea1d401e4f4b44ebf80df7b
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-08-26 17:56:33 -0500

    Update linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
-module.exports = {
-  globals: {
-    __PATH_PREFIX__: true,
-  },
-  rules: {
-    "indent": ["error", 2],
-    "no-console": "off",
-    "strict": ["error", "global"],
-    "curly": "warn",
-    "semi": ["error", "never"],
-    "space-in-parens": ["error", "never"],
-    "space-before-function-paren": ["error", "always"],
-    "space-before-blocks": ["error", "always"]
-  }
-}
+export const globals = {
+  __PATH_PREFIX__: true,
+};
+export const rules = {
+  "indent": ["error", 2],
+  "no-console": "off",
+  "strict": ["error", "global"],
+  "curly": "warn",
+  "semi": ["error", "never"],
+  "space-in-parens": ["error", "never"],
+  "space-before-function-paren": ["error", "always"],
+  "space-before-blocks": ["error", "always"]
+};
+export const env = {
+  "es6": true,
+};
+export const parserOptions = {
+  "ecmaVersion": "latest",
+};
