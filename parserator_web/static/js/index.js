ea4c650b4014157827318d69494c169126543cf5
Adam Callanan <a.callanan@sbcglobal.net>
2022-08-25 19:01:09 -0500

    begin scripting the parser

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,8 +1,41 @@
-/* TODO: Flesh this out to connect the form to the API and render results
+/* Connects the form to the API and render results
    in the #address-results div. */
 
-   $('.form').on('submit', function () {
-      var address = document.getElementById('address').value;
-      alert(`Submitted form with: ${address}`);
-  });
-  
+$('.form').on('submit', function (event) {
+   // Prevent default form submission behavior
+   event.preventDefault();
+
+   var address = getAddress();
+   submitAddress(address);
+
+});
+
+function getAddress() {
+   var address = $("#address").val().trim();
+   console.log(`Submitted form with: ${address}`);
+   return address;
+}
+
+async function submitAddress(address) {
+   try {
+      // TODO submit results to API properly
+      var res = await fetch("api/parse?address=" + address);
+      var data = await res.json();
+      console.log(res);
+      renderAddress("Address Type", []);
+   } catch (error) {
+      console.log(error);
+   }
+}
+
+function renderAddress(addressType, addressComponents) {
+   // TODO render results in the #address-results div
+
+   // Write Address Type text
+   $("#parse-type").text(addressType);
+
+   // TODO Write Address Components table
+
+   // Set Address Results div to show
+   $("#address-results").css("display", '');
+}
