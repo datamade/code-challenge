f683d02087af8c179f2240cc319f9bdc44cfd452
Adam Callanan <a.callanan@sbcglobal.net>
2022-08-25 22:09:21 -0500

    fix lint errors in js

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,81 +1,85 @@
 /* Connects the form to the API and render results
    in the #address-results div. */
 
+'use strict'
+
 $('.form').on('submit', function (event) {
-   event.preventDefault();
-   submitAddress();
-});
+  event.preventDefault()
+  submitAddress()
+})
 
-function getAddress() {
-   var address = $("#address").val();
-   return address.trim().replaceAll(" ", "+");
+function submitAddress () {
+  var address = getAddress()
+  var url = "api/parse?address=" + getAddress()
+  if (address === "") {
+    writeError("Please enter an address.")
+    return
+  }
+  try {
+    $.get(url, function (data) {
+      writeResults(data.address_type, data.address_components)
+    }).fail(function (data) {
+      writeError(data.responseJSON.detail)
+    })
+  } catch (error) {
+    writeError("Something went wrong, please try again.")
+  }
 }
 
-function submitAddress() {
-   var address = getAddress();
-   var url = "api/parse?address=" + getAddress();
-   if (address === "") {
-      writeError("Please enter an address.");
-      return;
-   }
-   try {
-      $.get(url, function (data) {
-         writeResults(data.address_type, data.address_components);
-      }).fail(function (data) {
-         writeError(data.responseJSON.detail);
-      })
-   } catch (error) {
-      writeError("Something went wrong, please try again.");
-   }
+function getAddress () {
+  var address = $("#address").val()
+  return address.trim().replaceAll(" ", "+")
 }
 
-function writeResults(addressType, addressComponents) {
-   writeTextById("parse-type", addressType);
-   writeAddressComponents(addressComponents);
-   showSuccess();
+function writeResults (addressType, addressComponents) {
+  writeTextById("parse-type", addressType)
+  writeAddressComponents(addressComponents)
+  showSuccess()
 }
 
-function writeAddressComponents(addressComponents) {
-   $("#address-parts").empty();
-   for (var component in addressComponents) {
-      if (Object.prototype.hasOwnProperty.call(addressComponents, component)) {
-         var row = '<tr><td>' + addressComponents[component] + '</td><td>' + component + '</td></tr>';
-         $("#address-parts").append(row);
-      }
-   }
+function writeAddressComponents (addressComponents) {
+  $("#address-parts").empty()
+  for (var component in addressComponents) {
+    if (Object.prototype.hasOwnProperty.call(addressComponents, component)) {
+      var addressPart = addressComponents[component]
+      var tag = component
+      var row = '<tr><td>' + addressPart + '</td><td>' + tag + '</td></tr>'
+      $("#address-parts").append(row)
+    }
+  }
 }
 
-function writeError(message) {
-   writeTextById("error-message", message);
-   showError();
+function writeError (message) {
+  writeTextById("error-message", message)
+  showError()
 }
 
-function showSuccess() {
-   hide("error");
-   show("success");
-   show("address-results");
+function showSuccess () {
+  hide("error")
+  show("success")
+  show("address-results")
 }
 
-function showError() {
-   hide("success");
-   show("error");
-   show("address-results");
+function showError () {
+  hide("success")
+  show("error")
+  show("address-results")
 }
 
-function show(id) {
-   showOrHideById(id, true)
+function show (id) {
+  showOrHideById(id, true)
 }
 
-function hide(id) {
-   showOrHideById(id, false)
+function hide (id) {
+  showOrHideById(id, false)
 }
 
-function showOrHideById(id, show) {
-   var setting = show ? '' : 'none';
-   $("#" + id).css("display", setting);
+function showOrHideById (id, show) {
+  var setting = show ? '' : 'none'
+  $("#" + id).css("display", setting)
 }
 
 
-function writeTextById(id, text) {
-   $("#" + id).text(text);
+function writeTextById (id, text) {
+  $("#" + id).text(text)
 }
