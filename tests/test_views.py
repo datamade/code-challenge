6de240358e95b1ce6fd0e591ecbb60a62b394a76
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-08-25 19:23:43 -0500

    Add sad path test

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,13 +1,12 @@
 import pytest
+import usaddress
 from pprint import pprint
-
 
 
 def test_api_parse_succeeds(client):
     # TODO: Finish this test. Send a request to the API and confirm that the
     # data comes back in the appropriate format.
     address_string = '3733 Southport Ave, Chicago, IL'
-    pprint('About to call client')
     response = client.get(f"/api/parse/?address={address_string}")
 
     expected_response = {
@@ -22,17 +21,13 @@ def test_api_parse_succeeds(client):
     }
 
     assert expected_response == response.json()
-    # should expect to get back 
-    # (OrderedDict([('AddressNumber', '123'),
-    #               ('StreetName', 'Main'),
-    #               ('StreetNamePostType', 'Street'),
-    #               ('PlaceName', 'Chicago'),
-    #               ('StateName', 'IL')]),
-    #               'Street Address')
 
 
-# def test_api_parse_raises_error(client):
-#     # TODO: Finish this test. The address_string below will raise a
-#     # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
-#     address_string = '123 main st chicago il 123 main st'
-#     pytest.fail()
+def test_api_parse_raises_error(client):
+    # TODO: Finish this test. The address_string below will raise a
+    # RepeatedLabelError, so ParseAddress.parse() will not be able to parse it.
+    address_string = '123 main st chicago il 123 main st'
+    response = client.get(f"/api/parse/?address={address_string}")
+
+    assert response.json()["error"] == "Sorry, we couldn't parse this address. Please check your address and try again!"
+
