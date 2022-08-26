49e9cfd266cc07fc7f1ce00bddf63e2abe56a289
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-08-26 11:03:38 -0500

    Change fetch to async await, add function to check user input

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,36 +1,40 @@
-/* TODO: Flesh this out to connect the form to the API and render results
-   in the #address-results div. */
 const submitButton = document.querySelector(".btn")
 const addressInput = document.querySelector(".form-control")
 const addressResults = document.querySelector(".address-results")
+const resultsTable = document.querySelector(".results-table")
 
+/** check if input is correct type before sending query
+  * @param {string} input - user's address input
+  * @returns {boolean} - is the input longer than 0
+  * 
+  * */    
 
-//function to take in user input and send to api
-
-const sendAddress = (userInput) => {
-   return fetch(`http://localhost:8000/${userInput}`)
-      .then(response => {
-         if (!response.ok) {
-            throw Error(response.statusText)
-         } else {
-            return response.json()
-
-         }
-      })
-      .catch(err => displayError(err))
+const checkInput = input => {
+   return input.length
 }
+
+const fetchAddress = async (userInput) => {
+   const response = await fetch(`api/parse/?address=${userInput}`)
+   const body = await response.json()
+   return body
+}
+
 
 
 const displayError = () => {
    //add error area in HTML?
 }
 
+//if response is 200, map result array values and tags as <tr> <td>value</td> <tag>tag</tag> </tr>
+
 
 const displayResults = (e) => {
    e.preventDefault()
    if (Event.target == submitButton) {
       sendAddress(addressInput.value)
-      addressResults.style.display = "block"
+         .then((result) => {
+
+         })
    }
 }
 
