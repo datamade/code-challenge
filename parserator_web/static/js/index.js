05720af88a9a82f64953a8fc2ae7a7b05e9840dd
Emma Brooke-Davidson <ecbrooked@gmail.com>
2022-08-26 11:24:02 -0500

    Add function to map parsed address elements, add function to toggle visibility

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -5,13 +5,17 @@ const resultsTable = document.querySelector(".results-table")
 
 /** check if input is correct type before sending query
   * @param {string} input - user's address input
-  * @returns {boolean} - is the input longer than 0
-  * 
-  * */    
+  * @returns {boolean} - is the input longer than 0 
+  */
 
 const checkInput = input => {
    return input.length
 }
+
+/**
+ * @param {string} userInput - user's address input
+ * @returns {Promise<void>} - async call parse input address using await to parse response body
+ */
 
 const fetchAddress = async (userInput) => {
    const response = await fetch(`api/parse/?address=${userInput}`)
@@ -19,13 +23,45 @@ const fetchAddress = async (userInput) => {
    return body
 }
 
+/**
+ * @param {Object} addressComponents - results from query
+ * key - component tag, value - parsed input section
+ * @returns {string} - html table sections, joined into string
+ */
 
+const formatParsedAddress = addressComponents => {
+   const components = Object.keys(addressComponents)
+   const componentHTML = components.map(component => 
+      `
+      <tr>
+         <th>${addressComponents[component]}</th>
+         <th>${component}</th>
+      </tr>
+      `
+   )
+   return componentHTML.join('')
+}
+
+const setInnerHTML = (id, insert) => {
+   const element = document.querySelector(id)
+   element.innerHTML = insert
+}
+
+/**
+ * 
+ * @param {boolean} isShown - should table be displayed? 
+ * @param {string} id - id/class used to query select element from html
+ * updates the dom, doesn't return anything
+ */
+
+const toggleView = (isShown, id) => {
+   const showDiv = document.querySelector(id)
+   showDiv.style = isShown ? "" : "display:none"
+}
 
 const displayError = () => {
    //add error area in HTML?
 }
-
-//if response is 200, map result array values and tags as <tr> <td>value</td> <tag>tag</tag> </tr>
 
 
 const displayResults = (e) => {
