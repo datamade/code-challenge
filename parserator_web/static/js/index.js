5c0b84a0dd3421c41ff840cd1cff61496cd33ad7
Brent Oakes <boakes@users.noreply.github.com>
2022-08-29 14:52:58 -0500

    Have basic api requests handled, still need to process errors

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,22 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+$(document).ready(function() {
+   var frm = $("#addrForm");
+   frm.submit(function(e){
+      e.preventDefault();
+      var urlAddr =frm.attr('action');
+      $.ajax({
+         url: urlAddr,
+         type: "GET",
+         data: frm.serialize(),
+         success: function(data)
+         {
+            alert(data);
+         },
+         error: function(data)
+         {
+            alert(data);
+         },
+      });
+   });
+});
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -16,9 +16,14 @@ class AddressParse(APIView):
     def get(self, request):
         # TODO: Flesh out this method to parse an address string using the
         # parse() method and return the parsed components to the frontend.
-        return Response({})
+        input_string = request.query_params["address"];
+        address_components, address_type = self.parse(input_string);
+        return Response({'input_string': input_string,'address_components': address_components, 'address_type': address_type})
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
+        parsedTuple = usaddress.tag(address)
+        address_components = parsedTuple[0]
+        address_type = parsedTuple[1]
         return address_components, address_type
