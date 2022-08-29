6b8f333648200c9a3896dc7eab3ca6284b12b405
nm-woodward <nowoodward15@gmail.com>
2022-08-28 17:35:54 -0700

    Completed Step 3 (wiring up form to send request to API). Also fixed allowed_hosts issue (self-induced) via navigating to localhost instead of default (0.0.0.0)

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -1,2 +1,12 @@
 /* TODO: Flesh this out to connect the form to the API and render results
    in the #address-results div. */
+const form = document.querySelector("form");
+
+form.addEventListener("submit", (event) => {
+  const addressSubmission = form.querySelector("#address").value;
+  console.log(addressSubmission);
+
+  fetch("api/parse/?address='" + addressSubmission + "'")
+    .then((response) => response.json())
+    .then((data) => console.log(data));
+});
