ef6ac66f8d73cf840f0be86b0785bd2fbfa3224e
nm-woodward <nowoodward15@gmail.com>
2022-08-28 19:15:22 -0700

    Completed Step 4 (display results from address request via index.html. Completed error handling display for RepeatedLabelError

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -2,11 +2,44 @@
    in the #address-results div. */
 const form = document.querySelector("form");
 
+/* Add event listener for form submission: send requested address to API, 
+unhide the Parsing Results table, and display result */
 form.addEventListener("submit", (event) => {
-  const addressSubmission = form.querySelector("#address").value;
-  console.log(addressSubmission);
+  // Prevent page from reloading on form submission
+  event.preventDefault();
 
+  // Grab address input from form submission
+  const addressSubmission = form.querySelector("#address").value;
+
+  // Send request to API with submitted address
   fetch("api/parse/?address='" + addressSubmission + "'")
     .then((response) => response.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+      // TEST
+      console.log(data.address_parts);
+
+      // Grab "address type" span and add API reponse text (data.address_type)
+      const addressTypeSpan = document.getElementById("parse-type");
+      addressTypeSpan.textContent = data.address_type;
+
+      // Grab the body of the address results table and add each key/value pair as a row
+      const addressResultTable = document.querySelector("tbody");
+      // Clear any existing rows from a prior search
+      addressResultTable.innerHTML = "";
+
+      if (data.address_type != "Error") {
+        for ([key, val] of Object.entries(data.address_parts)) {
+          const addressPartRow = document.createElement("tr");
+          addressPartRow.innerHTML = `<td>${String(val)}</td><td>${String(
+            key
+          )}</td>`;
+
+          addressResultTable.appendChild(addressPartRow);
+        }
+      } else {
+        alert(
+          `RepeatedLabel Error (see: https://usaddress.readthedocs.io/en/latest/) ${data.address_parts}`
+        );
+      }
+    });
 });
