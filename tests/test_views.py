81f508d78116192920db5823481c878b131251ca
Brent Oakes <boakes@users.noreply.github.com>
2022-09-14 18:39:10 -0500

    Changed Test API

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,18 +1,18 @@
 import json
+import usaddress
 from django.urls import reverse
 
 
 def test_api_parse_succeeds(client):
     address_string = '123 main st chicago il'
-    address_list = address_string.split(" ")
     url = reverse('address-parse')
     response = client.get(url, {'address': address_string})
     parsedResponse = json.loads(response.content.decode('utf8'))
+    apiParse = usaddress.tag(address_string)
     assert response.status_code == 200
     assert parsedResponse['input_string'] == address_string
-    assert parsedResponse['address_type'] == 'Street Address'
-    for piece in address_list:
-        assert piece in parsedResponse['address_components'].values()
+    assert parsedResponse['address_type'] == apiParse[1]
+    assert parsedResponse['address_components'] == apiParse[0]
 
 
 def test_api_parse_raises_error(client):
