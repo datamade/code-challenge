a6f3271c0ac4189972819d9eda4570ba921fbd9e
Ami Tian <81931093+amitian@users.noreply.github.com>
2022-09-14 21:55:51 -0400

    changes based on review comments

--- parserator_web/static/js/index.js
+++ parserator_web/static/js/index.js
@@ -5,25 +5,38 @@ var divblock = document.getElementById("address-results");
 var form = document.querySelector(".form");
 var t = document.getElementById('table-body');
 var parseType = document.getElementById('parse-type');
+var addressString = document.getElementById('address-string');
+
 
 // when the form is submitted, send API request
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  
+  // if the address input is an empty string
+  if(document.getElementById("address").value == '') {
+    alert('Hey! You forgot to type an address in the box!')
+    return;
+  }
 
   let apiurl = '/api/parse/' + '?address=' + document.getElementById("address").value;
   fetch(apiurl)
-  .then((response) => response.json())
-  .then((data) => {
+  .then((response) => {
+    var status = response.status;
+    console.log(status);
     let rowhtml = "";
-    //if request is successful
-    if((data.Address)){
-      divblock.style = "display: inline-block";
-      let address = data['Address'];
-      let addressType = data['Address Type'];
-      parseType.innerText = addressType;
-      let addressComponents = data['Address Components'];
-      // creates a new table row for every key, value pair in the addressComponents dict
-      for (const [key, value] of Object.entries(addressComponents)) {
+
+    // if request is successful
+    if (status==200) { 
+      return response.json()
+      .then(data => {
+        divblock.style = "display: inline-block";
+        let address = data['Address'];
+        let addressType = data['Address Type'];
+        let addressComponents = data['Address Components'];
+
+        parseType.innerText = addressType;
+        addressString.innerText = address;
+        for (const [key, value] of Object.entries(addressComponents)) {
           rowhtml += `
           <tr>
             <td>${key}</td>
@@ -31,23 +44,31 @@ form.addEventListener("submit", (event) => {
           </tr>
         `;
       };
-    } else{ // if request is unsuccessful
-      divblock.style = "display: inline-block";
-      let error_desc = data['Error Description']
-      let addressType = `Not found. ${error_desc} See parsing attempt below:`;
-      parseType.innerText = addressType;
-      let parsedString = data['Parsed String'];
-      
-      for (const [key, value] of Object.entries(parsedString)) {
-        rowhtml += `
-        <tr>
-          <td>${value[0]}</td>
-          <td>${value[1]}</td>
-        </tr>
-      `;
-    };
-  };
-  t.innerHTML = rowhtml;
+      t.innerHTML = rowhtml;
+    });
+    } else { // if request is not successful
+      return response.json()
+      .then(data => {
+        divblock.style = "display: inline-block";
+        let error_desc = data['error']
+        let addressType = `Not found. ${error_desc}`; // replaces addressType with error message
+        let address = data['Original String'];
+        let parsedString = data['Parsed String'];
+        for (const [key, value] of Object.entries(parsedString)) {
+          rowhtml += `
+          <tr>
+            <td>${value[0]}</td>
+            <td>${value[1]}</td>
+          </tr>
+          `;
+        };
+        parseType.innerText = addressType; 
+        addressString.innerText = address;
+        t.innerHTML = rowhtml; 
+      }); 
+    }; 
+    
 });
-
+event.target.reset(); // reset address form after submitting
+addressString.innerText = ''; 
 })
--- parserator_web/views.py
+++ parserator_web/views.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.renderers import JSONRenderer
-from rest_framework.exceptions import ParseError # why is this used?
+
 
 
 class Home(TemplateView):
@@ -25,24 +25,17 @@ class AddressParse(APIView):
                 'Address Type': address_type,
                 'Address Components': address_components,
                 }, status=200)
-        except:
-            original_string, parsed_string, error_desc = self.parse(input_string)
-            
-            return Response({
-                'Original String': original_string,
-                'Parsed String': parsed_string,
-                'Error Description': error_desc,
+        except usaddress.RepeatedLabelError as e:
+            return Response({'error': e.message,
+            'Original String': e.original_string,
+            'Parsed String' : e.parsed_string
             }, status=400)
 
 
     def parse(self, address):
         # TODO: Implement this method to return the parsed components of a
         # given address using usaddress: https://github.com/datamade/usaddress
-        try:
-            address_components, address_type = usaddress.tag(address)[0], usaddress.tag(address)[1]
-
-        except usaddress.RepeatedLabelError as e:
-            error_desc = "Not a valid address!"
-            return e.original_string, e.parsed_string, error_desc
-
+        address_components, address_type = usaddress.tag(address)[0], usaddress.tag(address)[1]
         return address_components, address_type
+
+        
